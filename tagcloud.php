a5826d6ef20ce472ddadd3302f16924ca8303622
yafp <fidel@yafp.de>
2016-02-03 19:16:56 +0100

    20160203 - Marking as WORK IN PROGRESS

--- tagcloud.php
+++ tagcloud.php
@@ -72,7 +72,7 @@
 			<div class="panel-group" id="accordion">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Tagcloud configuration</a></h4>
+						<h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">WIP - Tagcloud configuration</a></h4>
 					</div>
 					<div id="collapse1" class="panel-collapse collapse in">
 						<div class="panel-body">
--- timeline.php
+++ timeline.php
@@ -89,7 +89,7 @@
 			<div class="panel-group" id="accordion">
 				<div class="panel panel-default">
 					<div class="panel-heading">
-						<h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Timeline</a></h4>
+						<h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordion" href="#collapse1">WIP - Timeline</a></h4>
 					</div>
 					<div id="collapse1" class="panel-collapse collapse in">
 						<div class="panel-body">
