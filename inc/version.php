9205dd5b9e7bc5a94c4c1af7766683e0adb35750
yafp <fidel@yafp.de>
2016-02-15 21:16:15 +0100

    20160215 - Minor changes regarding nav

--- inc/version.php
+++ inc/version.php
@@ -4,5 +4,5 @@
 	// ########################################################################################
 	$appname		= "clemStats";
 	$tagline		= "a clementine database analyzer";
-	$version 		= "20160209.01";
+	$version 		= "20160215.01";
 ?>
