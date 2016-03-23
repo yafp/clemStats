8273f9b41471a5470272a6180bf6ef9c9b094949
yafp <fidel@yafp.de>
2016-03-23 21:15:30 +0100

    20160323 - Updating version stuff

--- inc/version.php
+++ inc/version.php
@@ -4,5 +4,5 @@
 	// ########################################################################################
 	$appname		= "clemStats";
 	$tagline		= "a clementine database analyzer";
-	$version 		= "20160215.01";
+	$version 		= "20160323.01";
 ?>
