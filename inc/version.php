48b6968cec5d8a80edf7693fb10eebdba60e12cd
yafp <fidel@yafp.de>
2016-04-09 00:07:08 +0200

    20160409 - Moving to 20160409.03

--- inc/version.php
+++ inc/version.php
@@ -4,5 +4,5 @@
 	// ########################################################################################
 	$appname		= "clemStats";
 	$tagline		= "a clementine database analyzer";
-	$version 		= "20160408.02";
+	$version 		= "20160408.03";
 ?>
